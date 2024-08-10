8828b0cb06dad9ce6c336fa711df66957d32c7bb
Monica Hong <monicaaprilhong@gmail.com>
2024-08-09 21:49:16 -0700

    superuser and admin portal

--- AdoptMeow/adopt_meow_app/admin.py
+++ AdoptMeow/adopt_meow_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Adoption, Pet, User
+
+
+class PetAdmin(admin.ModelAdmin):
+  list_display = ('name', 'age', 'breed', 'adoption_status')
+  search_fields = ('name', 'breed')
+  list_filter = ('adoption_status', 'breed')
+
+admin.site.register(User)
+admin.site.register(Pet, PetAdmin)
+admin.site.register(Adoption)
